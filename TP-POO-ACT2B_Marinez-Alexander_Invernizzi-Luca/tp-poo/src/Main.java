ca59f575db18960512fbb2e4558e8676aaf0c131
luca-invernizzi-davinci <89217721+luca-invernizzi-davinci@users.noreply.github.com>
2021-12-06 00:19:08 -0300

    Delete TP-POO-ACT2B_Marinez-Alexander_Invernizzi-Luca directory

--- TP-POO-ACT2B_Marinez-Alexander_Invernizzi-Luca/tp-poo/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-
-        CuentaBancaria cuenta1 = new CuentaBancaria("Luca Invernizzi", "43160763");
-        cuenta1.mostrarMenu();
-    }
-}
