a1aef363529ad1ca7be1deb64c3a5f054e915892
luca-invernizzi-davinci <89217721+luca-invernizzi-davinci@users.noreply.github.com>
2021-12-05 23:28:50 -0300

    Add files via upload

---
+++ TP-POO-ACT2B_Marinez-Alexander_Invernizzi-Luca/tp-poo/src/CuentaBancaria.java
@@ -0,0 +1,96 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CuentaBancaria {
+
+    //Atributos
+    String nombreCliente;
+    String dniCliente;
+    double saldo;
+    double ultimoMovimiento;
+    String opcion;
+
+    //Constructores
+    public CuentaBancaria(String nombreCliente, String dniCliente) {
+        this.nombreCliente = nombreCliente;
+        this.dniCliente = dniCliente;
+    }
+
+    public CuentaBancaria(String nombreCliente, String dniCliente, double saldo) {
+        this.nombreCliente = nombreCliente;
+        this.dniCliente = dniCliente;
+        this.saldo = saldo;
+    }
+
+    //Metodos
+    public void depositar(double cantidad) {
+        if (cantidad > 0) {
+            saldo = saldo + cantidad;
+            ultimoMovimiento=cantidad;
+        }
+    }
+
+    public void retirar(double cantidad) {
+        if (cantidad > 0) {
+            saldo = saldo - cantidad;
+            ultimoMovimiento=cantidad;
+        }
+        if (saldo < cantidad){
+            System.out.println("Sos pobre, no tenes plata \n");
+        }
+    }
+
+    public void consultarUltimoMovimiento() {
+        if (ultimoMovimiento > 0){
+            System.out.println("Cantidad depositada: " + ultimoMovimiento + "\n");
+        } else if (ultimoMovimiento < 0) {
+            System.out.println("Cantidad retirada: " + ultimoMovimiento + "\n");
+        } else if (ultimoMovimiento == 0) {
+            System.out.println("No se han realizado transacciones \n");
+        }
+    }
+
+    public void mostrarMenu (){
+        System.out.println("Bienvenido " + nombreCliente);
+        System.out.println("Ingrese una opción:\n" +
+                "Opción A: Consultar Saldo.\n" +
+                "Opción B: Depositar\n" +
+                "Opción C: Retirar\n" +
+                "Opción D: Consultar último movimiento\n" +
+                "Opción E: Salir.");
+        Scanner sc = new Scanner(System.in);
+        String opcion;
+        opcion = sc.nextLine();
+        switch (opcion.toUpperCase()) {
+            case "A":
+                System.out.println("Su saldo es: " + saldo);
+                break;
+            case "B":
+                Scanner scb = new Scanner(System.in);
+                double deposito;
+                System.out.println("Ingrese cantidad a depositar");
+                deposito = scb.nextDouble();
+                depositar(deposito);
+                mostrarMenu();
+                break;
+            case "C":
+                Scanner scc = new Scanner(System.in);
+                double retiro;
+                System.out.println("Ingrese cantidad a retirar");
+                retiro = scc.nextDouble();
+                retirar(retiro);
+                mostrarMenu();
+                break;
+            case "D":
+                consultarUltimoMovimiento();
+                mostrarMenu();
+                break;
+            case "E":
+                System.out.println("Hasta luego, tenga un buen día");
+                break;
+            default:
+                System.out.println("Opción inválida, intente de nuevo");
+                mostrarMenu();
+        }
+    }
+}
---
+++ TP-POO-ACT2B_Marinez-Alexander_Invernizzi-Luca/tp-poo/src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CuentaBancaria cuenta1 = new CuentaBancaria("Luca Invernizzi", "43160763");
+        cuenta1.mostrarMenu();
+    }
+}
